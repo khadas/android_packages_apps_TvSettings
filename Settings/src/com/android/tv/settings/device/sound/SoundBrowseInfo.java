cd98d976c47c652daa600b979a71e79168a9792a
Jianqin Zhang <jianqin.zhang@amlogic.com>
2014-11-28 13:54:27 +0800

    Add digital sound output setting
    Change-Id: I48a62b9384192b7118b481764c90b6a5db906029

---
+++ Settings/src/com/android/tv/settings/device/sound/SoundBrowseInfo.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tv.settings.device.sound;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v17.leanback.widget.ArrayObjectAdapter;
+import android.support.v17.leanback.widget.HeaderItem;
+
+import com.android.tv.settings.BrowseInfoBase;
+import com.android.tv.settings.MenuItem;
+import com.android.tv.settings.R;
+import com.android.tv.settings.SettingsConstant;
+
+/**
+ * Gets the list of browse headers and browse items.
+ */
+public class SoundBrowseInfo extends BrowseInfoBase {
+
+    private static final boolean WIFI_DISPLAY_SUPPORTED = false;
+    private static final int HEADER_ID = 1;
+    private final Context mContext;
+    private final ArrayObjectAdapter mRow = new ArrayObjectAdapter();
+    private int mNextItemId;
+
+    SoundBrowseInfo(Context context) {
+        mContext = context;
+        mNextItemId = 0;
+        mHeaderItems.add(new HeaderItem(HEADER_ID, mContext.getString(R.string.device_sound), null));
+        mRows.put(HEADER_ID, mRow);
+    }
+
+    @Override
+    public void refreshContent() {
+        init();
+    }
+
+    void init() {
+        mRow.clear();
+        mRow.add(new MenuItem.Builder().id(mNextItemId++)
+                .title(mContext.getString(R.string.device_sound_effects))
+                .imageResourceId(mContext, R.drawable.ic_settings_sound_on)
+                .intent(getIntent(SettingsConstant.PACKAGE,
+                        SettingsConstant.PACKAGE + ".device.sound.systemsound.SystemSoundActivity"))
+                .build());
+
+        if (SettingsConstant.needDroidlogicMboxFeature(mContext)) {
+            mRow.add(new MenuItem.Builder().id(mNextItemId++)
+                    .title(mContext.getString(R.string.device_sound_digital))
+                    .imageResourceId(mContext, R.drawable.ic_settings_sound_on)
+                    .intent(getIntent(SettingsConstant.PACKAGE,
+                            SettingsConstant.PACKAGE + ".device.sound.digitalsound.DigitalSoundActivity"))
+                    .build());
+        }
+    }
+
+    private Intent getIntent(String targetPackage, String targetClass) {
+        ComponentName componentName = new ComponentName(targetPackage, targetClass);
+        Intent i = new Intent();
+        i.setComponent(componentName);
+        return i;
+    }
+}
