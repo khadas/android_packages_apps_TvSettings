12beaf31aefdde30c6be9ad3aefafdb77dfeb9e9
Jianqin Zhang <jianqin.zhang@amlogic.com>
2015-05-15 15:55:09 +0800

    Modify the Serial_number to Kernel_version
    Change-Id: Ie45d724e460d121f6a62d018264db7ccc4d4bee1

--- Settings/src/com/android/tv/settings/about/AboutActivity.java
+++ Settings/src/com/android/tv/settings/about/AboutActivity.java
@@ -42,6 +42,11 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -52,6 +57,7 @@ public class AboutActivity extends DialogActivity implements ActionAdapter.Liste
         ActionAdapter.OnFocusListener {
 
     private static final String TAG = "AboutActivity";
+    private static final String FILENAME_PROC_VERSION = "/proc/version";
 
     /**
      * Action keys for switching over in onActionClicked.
@@ -243,9 +249,9 @@ public class AboutActivity extends DialogActivity implements ActionAdapter.Liste
                 .enabled(true)
                 .build());
         actions.add(new Action.Builder()
-                .key("serial")
-                .title(getString(R.string.about_serial))
-                .description(Build.SERIAL)
+                .key("kernel_version")
+                .title(getString(R.string.about_kernel))
+                .description(getFormattedKernelVersion())
                 .enabled(false)
                 .build());
         actions.add(new Action.Builder()
@@ -290,5 +296,56 @@ public class AboutActivity extends DialogActivity implements ActionAdapter.Liste
             }
         }
         return null;  // No system image package found.
+    }
+
+    /**
+     * Reads a line from the specified file.
+     * @param filename the file to read from
+     * @return the first line, if any.
+     * @throws IOException if the file couldn't be read
+     */
+    private static String readLine(String filename) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filename), 256);
+        try {
+            return reader.readLine();
+        } finally {
+            reader.close();
+        }
+    }
+
+    public static String getFormattedKernelVersion() {
+        try {
+            return formatKernelVersion(readLine(FILENAME_PROC_VERSION));
+        } catch (IOException e) {
+            Log.e(TAG, "IO Exception when getting kernel version for Device Info screen", e);
+            return "Unavailable";
+        }
+    }
+
+    public static String formatKernelVersion(String rawKernelVersion) {
+        // Example (see tests for more):
+        // Linux version 3.0.31-g6fb96c9 (android-build@xxx.xxx.xxx.xxx.com) \
+        //     (gcc version 4.6.x-xxx 20120106 (prerelease) (GCC) ) #1 SMP PREEMPT \
+        //     Thu Jun 28 11:02:39 PDT 2012
+
+        final String PROC_VERSION_REGEX =
+            "Linux version (\\S+) " + /* group 1: "3.0.31-g6fb96c9" */
+            "\\((\\S+?)\\) " +        /* group 2: "x@y.com" (kernel builder) */
+            "(?:\\(gcc.+? \\)) " +    /* ignore: GCC version information */
+            "(#\\d+) " +              /* group 3: "#1" */
+            "(?:.*?)?" +              /* ignore: optional SMP, PREEMPT, and any CONFIG_FLAGS */
+            "((Sun|Mon|Tue|Wed|Thu|Fri|Sat).+)"; /* group 4: "Thu Jun 28 11:02:39 PDT 2012" */
+
+        Matcher m = Pattern.compile(PROC_VERSION_REGEX).matcher(rawKernelVersion);
+        if (!m.matches()) {
+            Log.e(TAG, "Regex did not match on /proc/version: " + rawKernelVersion);
+            return "Unavailable";
+        } else if (m.groupCount() < 4) {
+            Log.e(TAG, "Regex match on /proc/version only returned " + m.groupCount()
+                    + " groups");
+            return "Unavailable";
+        }
+        return m.group(2) + " " + m.group(3) + "\n" + // x@y.com #1
+            m.group(4);                            // Thu Jun 28 11:02:39 PDT 2012
     }
 }
