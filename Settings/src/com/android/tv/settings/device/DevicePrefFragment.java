bcc1bc89faa883359ff675b94dfbfb5bc0c101bb
terry <terry@szwesion.com>
2019-07-05 21:33:31 +0800

    Temporarily remove WOL and PCIE menu

--- Settings/src/com/android/tv/settings/device/DevicePrefFragment.java
+++ Settings/src/com/android/tv/settings/device/DevicePrefFragment.java
@@ -80,6 +80,14 @@ public class DevicePrefFragment extends SettingsPreferenceFragment {
         if (inputPref != null) {
             inputPref.setVisible(mInputSettingNeeded);
         }
+        final Preference wolPref = findPreference("wol");
+        if (wolPref != null) {
+            wolPref.setVisible(false);
+        }
+        final Preference portmodePref = findPreference("portmode");
+        if (portmodePref != null) {
+            portmodePref.setVisible(false);
+        }
     }
 
     @Override
